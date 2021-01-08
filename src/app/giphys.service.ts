693140a8121497317ad50a2776e47cc1c1d3f00c
unknown <pete.carey@yahoo.co.uk>
2021-01-08 17:11:01 +0000

    api amended in service file

--- src/app/giphys.service.ts
+++ src/app/giphys.service.ts
@@ -8,8 +8,8 @@ export class GiphysService {
   constructor(private http: HttpClient) {}
   getGiphys() {
     return this.http.get(
-      'https://hplussport.com/api/products'
-      // 'https://api.giphy.com/v1/gifs/random?api_key=EFhi0dLq6QrVRgTsnS65CaiwS20kO6rH&tag=squirrels&rating=g'
+      // 'https://hplussport.com/api/products'
+      'https://api.giphy.com/v1/gifs/random?api_key=EFhi0dLq6QrVRgTsnS65CaiwS20kO6rH&tag=squirrels&rating=g'
     );
   }
 }
